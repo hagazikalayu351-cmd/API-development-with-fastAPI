1f70384969a9cea1c6be5dc59ebcaf6da38ecae7
Hagazi Kalayu <hagazikalayu351@gmail.com>
2026-09-03 22:13:50 +0300

    Day 3

--- fast_api/main.py
+++ fast_api/main.py
@@ -1,10 +1,21 @@
 from fastapi import FastAPI
+from enum import Enum
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class Students(BaseModel):
+    name: str
+    age: int
+    grade: str      
+    phone: str| None = None
+
+
+
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
-@app.get("/items/{item_id}")
-def read_item(item_id:int, q:str|None = None):
-    return {"item_id": item_id, "q": q}
+async def root():
+    return {"enter the name of the student you want to add "}
+
+@app.post("/students")
+async def create_student(student: Students):
+    return student
